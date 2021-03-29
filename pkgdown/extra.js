5192cf61bd63855737909ca96b8c2d318510c03b
David J. Bosak <dbosak01@gmail.com>
2021-03-28 22:08:30 -0400

    Improved pkgdown site header.

---
+++ pkgdown/extra.js
@@ -0,0 +1,11 @@
+
+
+window.addEventListener('load', (event) => {
+
+    var br = document.getElementsByClassName("navbar-brand")[0]
+    
+    tg = '<span style="font-size:10pt">part of the <a href="https://r-sassy.org" style="font-family:verdana;font-weight:bold">sassy</a> system</span>'
+    
+    br.innerHTML = br.innerHTML + tg
+});
+
